4b981d92f86730d801eb8e5e48ff8090167eeb06
darmyway <118660345+darmyway@users.noreply.github.com>
2024-03-31 08:53:21 +0100

    listapp_103216.py

--- listapp_103216.py
+++ listapp_103216.py
@@ -24,10 +24,3 @@ if remita is not None:
     p_xtractor.drop_duplicates(inplace = True)
     p_xtractor  = p_xtractor.reset_index(drop = True)
     st.write(p_xtractor)
-    def download_csv(df):
-        csv = df.to_csv(index=False, encoding='utf-8-sig')  # Ensure UTF-8 encoding
-        b64 = base64.b64encode(csv.encode()).decode()  # B64 encoding for download
-        href = f'<a href="data:file/csv;base64,{b64}" download="extracted_file.csv">Download Extracted File Here</a>'
-        return href
-
-    st.markdown(download_csv(p_xtractor), unsafe_allow_html=True)
